bba8255d03598c89c6bf5d43bf1283321e60754c
Brian Juul Andersen <brianmanden@gmail.com>
2015-04-18 08:38:29 +0200

    ContactForm template semiokay

---
+++ umbracoassignment/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using umbracoassignment.ViewModels;
+
+namespace umbracoassignment.Controllers 
+{
+    public class ContactFormSurfaceController : Umbraco.Web.Mvc.SurfaceController
+    {
+        public ActionResult Index(){
+            return PartialView("ContactForm", new ContactForm());
+        }
+    }
+}
